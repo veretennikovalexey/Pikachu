f3501647aaf1d6ff5c47c978f8210104b68579af
alex <30378329+veretennikovalexey@users.noreply.github.com>
2021-10-31 21:56:53 +0300

    Add files via upload

--- mu_code/31.10.2021/range.py
+++ mu_code/31.10.2021/range.py
@@ -1,4 +1,7 @@
 for ii in range( 12, 16 ) :
-    print( ii, end = ' ' )
-# 12 13 14 15
+  print( ii, end = ' ' ) # 12 13 14 15
+print()
+for ii in range( 0, 10, 2 ) :
+  print( ii, end = ' ' ) # 0 2 4 6 8
+
 
