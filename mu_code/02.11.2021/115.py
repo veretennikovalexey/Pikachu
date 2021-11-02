2ae69b3bee36f6bdb88e2e0b1e3e2a5805adfcab
alex <30378329+veretennikovalexey@users.noreply.github.com>
2021-11-02 15:20:06 +0300

    Add files via upload

---
+++ mu_code/02.11.2021/115.py
@@ -0,0 +1,15 @@
+def spam() :
+  global eggs
+  eggs = 'spam' # это глобальная переменная
+
+def bacon() :
+  eggs = 'bacon' # это локальная переменная
+
+def ham() :
+  print( eggs ) # это глобальная переменная
+
+eggs = 42       # это глобальная переменная
+spam()
+print( eggs ) # в результате будет 'spam'
+
+
