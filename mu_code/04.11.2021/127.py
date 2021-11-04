12f05f2d2ffd423a64f2e243bfddbdb3d8977f11
alex <30378329+veretennikovalexey@users.noreply.github.com>
2021-11-04 13:44:45 +0300

    Add files via upload

---
+++ mu_code/04.11.2021/127.py
@@ -0,0 +1,7 @@
+spam = [ 'кот', 'мышь', 'крыса', 'слон' ]
+spam[ 10000 ]
+
+# Traceback (most recent call last):
+#   File "c:\users\os\desktop\04.11.2021\127.py", line 2, in <module>
+#     spam[ 10000 ]
+# IndexError: list index out of range
