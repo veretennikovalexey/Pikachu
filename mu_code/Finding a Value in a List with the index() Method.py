1b318ddd0b83a8058195b869837ca6427e870dae
alex <30378329+veretennikovalexey@users.noreply.github.com>
2021-11-08 09:22:57 +0300

    Add files via upload

---
+++ mu_code/Finding a Value in a List with the index() Method.py
@@ -0,0 +1,5 @@
+spam = [ 'hello', 'hi', 'howdy', 'heyas' ]
+print( spam.index( 'hello' ) ) # 0
+print( spam.index( 'heyas' ) ) # 3
+spam = [ 'Zophi', 'Pooka', 'Fat-tail', 'Pooka' ]
+print( spam.index( 'Pooka' ) ) # 1
