a5ec1daae5cc7cb7b393422f3d1ee08f896f5eb8
alex <30378329+veretennikovalexey@users.noreply.github.com>
2021-11-08 08:46:40 +0300

    Add files via upload

---
+++ mu_code/Using the enumerate() Function with Lists.py
@@ -0,0 +1,7 @@
+supplies = [ 'pens', 'staplers', 'flamethrowers', 'binders' ]
+for index, item in enumerate( supplies ) :
+  print ( f' index { index } in supplies is { item }' )
+# index 0 in supplies is pens
+# index 1 in supplies is staplers
+# index 2 in supplies is flamethrowers
+# index 3 in supplies is binders
