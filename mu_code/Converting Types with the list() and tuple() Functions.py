0cddb76a791fafe53c9a7b41fa892d27871635c8
alex <30378329+veretennikovalexey@users.noreply.github.com>
2021-11-08 11:29:07 +0300

    Add files via upload

--- mu_code/Converting Types with the list() and tuple() Functions.py
+++ mu_code/Converting Types with the list() and tuple() Functions.py
@@ -4,6 +4,7 @@ print( mytuple ) # ('cat', 'dog', 5)
 mylist = list( ( 'cat', 'dog', 5 ) )
 print( mylist ) # ['cat', 'dog', 5]
 
-print( list( 'hello' ) ) # ['h', 'e', 'l', 'l', 'o']
+print( list( 'hello' ) )  # ['h', 'e', 'l', 'l', 'o']
+print( tuple( 'hello' ) ) # ('h', 'e', 'l', 'l', 'o')
 
 
