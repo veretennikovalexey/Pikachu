7bfece35a944dd620afb2034863e95b20c0b2b24
alex <30378329+veretennikovalexey@users.noreply.github.com>
2021-11-09 09:41:22 +0300

    Add files via upload

---
+++ mu_code/The Dictionary Data Type/The Dictionary Data Type.py
@@ -0,0 +1,8 @@
+myCat = { 'size' : 'fat', 'color' : 'gray', 'disposition' : 'loud' }
+print( type( myCat ) )   # <class 'dict'>
+print( myCat[ 'size' ] ) # fat
+print( f'My cat has { myCat[ "color" ] } fur ' )
+
+# У моей кошки серая шерсть
+
+spam = { 12345 : 'Luggage Combination', 42 : 'The Answer' }
