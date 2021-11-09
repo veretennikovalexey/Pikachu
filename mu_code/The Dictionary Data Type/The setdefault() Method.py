e183e600cbae4ddeaec38e86013d626e15f2213c
alex <30378329+veretennikovalexey@users.noreply.github.com>
2021-11-09 12:01:55 +0300

    Add files via upload

--- mu_code/The Dictionary Data Type/The setdefault() Method.py
+++ mu_code/The Dictionary Data Type/The setdefault() Method.py
@@ -18,3 +18,7 @@ for character in message :
   count[ character ] = count[ character ] + 1
 
 print( count )
+
+# { 'I': 1, 't': 6, ' ': 13, 'w': 2, 'a': 4, 's': 3, 'b': 1,
+#   'r': 5, 'i': 6, 'g':  2, 'h': 3, 'c': 3, 'o': 2, 'l': 3,
+#   'd': 3, 'y': 1, 'n':  4, 'A': 1, 'p': 1, ',': 1, 'e': 5, 'k': 2 }
