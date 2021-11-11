f46611c859da77d9cd4c24059bf19f34c05747a9
alex <30378329+veretennikovalexey@users.noreply.github.com>
2021-11-11 11:17:13 +0300

    Add files via upload

--- mu_code/PATTERN MATCHING WITH REGULAR EXPRESSIONS/Finding Patterns of Text Without Regular Expressions.py
+++ mu_code/PATTERN MATCHING WITH REGULAR EXPRESSIONS/Finding Patterns of Text Without Regular Expressions.py
@@ -17,6 +17,13 @@ def isPhoneNumber( text ) :
 
   return True
 
+message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office'
+for ii in range ( len( message ) ) :
+  chunk = message[ ii : ii + 12 ]
+  if isPhoneNumber( chunk ) :
+    print( 'Phone number found: ' + chunk )
+print( 'Done' )
+
 print( 'Is 415-555-4242 a phone number ?' )
 print( isPhoneNumber( '415-555-4242' ) )
 print( 'Is Moshi moshi a phone number ?' )
