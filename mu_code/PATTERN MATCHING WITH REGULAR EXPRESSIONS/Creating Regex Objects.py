fe42df2d699a2f37cd422e5a6b0568d20901e3d2
alex <30378329+veretennikovalexey@users.noreply.github.com>
2021-11-11 12:30:39 +0300

    Add files via upload

---
+++ mu_code/PATTERN MATCHING WITH REGULAR EXPRESSIONS/Creating Regex Objects.py
@@ -0,0 +1,18 @@
+import re
+phoneNumRegex = re.compile( r'\d\d\d-\d\d\d-\d\d\d\d' )
+print( type( phoneNumRegex ) ) # <class 're.Pattern'>
+mo = phoneNumRegex.search( 'My number is 415-555-4242' )
+print( type( mo ) ) # <class 're.Match'>
+print( 'Phone number found: ' + mo.group() )
+
+'''
+using regular expression ( regex ) in Python
+
+1. Import the regex module with import re
+2. Create a regex object with re.compile() function
+3. Pass the string you want to search into the regex object's search() method
+   This return a Match object
+4. Call the Match object's group() method to return a string of actual matched text
+
+https://pythex.org/
+'''
