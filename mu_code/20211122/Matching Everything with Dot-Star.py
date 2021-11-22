8c1eea602ec55d765e86e0664c625c9009f73583
alex <30378329+veretennikovalexey@users.noreply.github.com>
2021-11-22 15:17:44 +0300

    Add files via upload

--- mu_code/20211122/Matching Everything with Dot-Star.py
+++ mu_code/20211122/Matching Everything with Dot-Star.py
@@ -1,4 +1,14 @@
 import re
+nongreedyRegex = re.compile( r'<.*?>' )
+mo = nongreedyRegex.search( '<To serve man> for dinner>' )
+print( mo.group() ) # <To serve man>
+greedyRegex = re.compile( r'<.*>' )
+mo = greedyRegex.search( '<To serve man> for dinner>' )
+print( mo.group() ) # <To serve man> for dinner>
+'''
+the dot - star uses greedy mode
+the dot - star - and - question uses non-greedy mode
+'''
 nameRegex = re.compile( r'First Name: (.*) Last Name: (.*)' )
 mo = nameRegex.search( 'First Name: Al Last Name: Sweigart' )
 print( mo.group( 1 ) ) # Al
