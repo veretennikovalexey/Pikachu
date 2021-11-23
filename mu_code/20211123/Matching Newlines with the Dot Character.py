12c58b916ff7b9017e0da2dbd7103a89ccb62e47
alex <30378329+veretennikovalexey@users.noreply.github.com>
2021-11-23 08:31:06 +0300

    Add files via upload

--- mu_code/20211123/Matching Newlines with the Dot Character.py
+++ mu_code/20211123/Matching Newlines with the Dot Character.py
@@ -1,3 +1,26 @@
+'''
+Review of regex symbols
+
+?     zero or one of the preceding group
+*     zero or more of the preceding group
++     one or more of the preceding group
+{n}   exacltly n of the preceding group
+{n,}  n or more of the preceding group
+{,m}  0 to m of the preceding group
+{n,m} matches at least n and at most m of the preceding group
+{n,m}? or *? or +? perfoms a non-greedy match of the preceding group
+^spam string must begin with spam
+spam$ string must end with spam
+.     any character, except newline
+\d    a digit
+\w    word
+\s    space
+\D    anything except a digit
+\W    anything except word character
+\S    anything except space character
+[abc] matches any charecter between the brackers ( such as a,b or c )
+[^abc] matches any charecter that isn't between the brackets
+'''
 import re
 newlineRegex = re.compile( '.*', re.DOTALL )
 aa = 'Serve the public trust\nProtect the innocent\nUphold the law'
